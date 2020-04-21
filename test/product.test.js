14d471fec72a2fcae45194d944b6e8785c4a2994
AnthoniaS <anthoniasr@gmail.com>
2020-04-21 20:00:43 -0300

    :bug: Corrige bug de baixa de estoque

--- test/product.test.js
+++ test/product.test.js
@@ -4,5 +4,5 @@ import sell from '../src/service/sellProduct'
 test('deve validar baixa de estoque da venda de uma unidade', () => {
     let produto = new Product('Celular', 500.00, 980.00, 10);
     sell(produto, 1);
-    expect(produto.stock).toBe(20);
+    expect(produto.stock).toBe(9);
 })
