f1c2b32bb598d28515aec3b55587c1a6e79c1930
AnthoniaS <anthoniasr@gmail.com>
2020-04-21 19:37:38 -0300

    :art: Base do projeto para o CircleCI

---
+++ test/product.test.js
@@ -0,0 +1,8 @@
+import Product from '../src/model/product'
+import sell from '../src/service/sellProduct'
+
+test('deve validar baixa de estoque da venda de uma unidade', () => {
+    let produto = new Product('Celular', 500.00, 980.00, 10);
+    sell(produto, 1);
+    expect(produto.stock).toBe(20);
+})
